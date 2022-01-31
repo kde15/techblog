20ea78e3f8c605bf246a79588ed46d45cd1a1497
kde15 <kde15150@gmail.com>
2022-01-31 20:30:20 +0900

    Change: Header css file location

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "styles/Header.module.css";
+import styles from "./Header.module.css";
 import { LoadingLink } from "components/utils/LoadingLink";
 
 export const Header = () => {
