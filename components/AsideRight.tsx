c62f07facb33f17cbf3bb0abb053e4709b75323c
kde15 <kde15150@gmail.com>
2022-02-20 06:00:17 +0900

    Chore: update profile text

--- components/AsideRight.tsx
+++ components/AsideRight.tsx
@@ -18,18 +18,9 @@ export const AsideRight = () => {
                 <p>
                     <strong>自己紹介</strong>
                 </p>
-                <Line>エンジニア実務経験3年の26歳です。</Line>
-                <Line>
-                    Javaのバックエンドを2年程やった後にReactを触り、フロントエンドに興味が湧いてプライベートでも勉強中です。
-                </Line>
-                <Line>
-                    Reactの他にはTypeScript、Go、Firebase、AWS
-                    Amplify、Lambda、CDKに興味があります。
-                </Line>
-                <Line>
-                    AWS CDK以外は実務・プライベートで使った経験があります。
-                </Line>
-                <Line>コンビニコーヒーと玄米茶が好きです。</Line>
+                <Line>エンジニア歴3年、フリーランス1年目の26歳男です。</Line>
+                <Line>今はAngularでフロントエンドをやってます。</Line>
+                <Line>今まではJavaとReactをやってました。</Line>
             </div>
         </aside>
     );
